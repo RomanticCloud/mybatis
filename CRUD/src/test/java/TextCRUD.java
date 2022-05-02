d685786a5500d4211044430fe695a7293347ab8a
wzp <2819645829@qq.com>
2022-05-03 00:33:19 +0800

    设置SqlSession工具类

--- CRUD/src/test/java/TextCRUD.java
+++ CRUD/src/test/java/TextCRUD.java
@@ -1,22 +1,16 @@
 import com.wang.mapper.StudentMapper;
 import com.wang.pojo.Student;
-import org.apache.ibatis.io.Resources;
+import com.wang.utils.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class TextCRUD {
 
     @Test
     public void textInsert() throws IOException {
-        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);
-
-        Student s = new Student(3, "王政普", 22, "男", "2819645829@qq.com");
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        Student s = new Student(4, "王政普", 22, "男", "2819645829@qq.com");
 
         StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
         int i = mapper.insertStudent(s);
